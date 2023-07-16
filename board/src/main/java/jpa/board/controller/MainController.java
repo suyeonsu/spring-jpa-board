fa90025ca0f94718ec49552e24f30303f7403212
suyeonsu <agew0423@daum.net>
2023-07-16 20:40:43 +0900

    init

---
+++ board/src/main/java/jpa/board/controller/MainController.java
@@ -0,0 +1,16 @@
+package jpa.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MainController {
+
+    @GetMapping("/main")
+    public String main(){
+        return "main/main";
+    }
+
+}
